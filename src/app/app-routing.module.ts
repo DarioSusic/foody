edf817ac5a9ecce3afe488b4b3c80c6ca7cdbffb
Dario Susic <dario.susic@gmail.com>
2022-02-28 22:06:04 +0100

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/dashboard/dashboard.module').then(module => module.DashboardModule) },
   { path: 'receipts', loadChildren: () => import('./pages/receipts/receipts.module').then(module => module.ReceiptsModule) },
   { path: 'meals', loadChildren: () => import('./pages/meals/meals.module').then(module => module.MealsModule) },
+  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(module => module.ProfileModule) },
+  { path: 'register', loadChildren: () => import('./pages/profile/register/register.module').then(module => module.RegisterModule) },
+  { path: 'login', loadChildren: () => import('./pages/profile/login/login.module').then(module => module.LoginModule) },
 
   // { path: 'profile', loadChildren: () => import('./main-page/main-page.module').then(module => module.MainPageModule) },
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -7,6 +8,19 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public isLoggedIn: boolean = false;
   title = 'foody';
   faCoffee = faCoffee;
+
+  constructor (private zone: NgZone, private router: Router) {
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/login') {
+          this.isLoggedIn = false;
+        } else {
+          this.isLoggedIn = true;
+        }
+      }
+    });
+  }
 }
